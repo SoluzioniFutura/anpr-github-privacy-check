3370e8e5e3de330ead9d512566bdcd0048810177
Matte Carra <matte.carra@gmail.com>
2017-10-08 04:52:12 +0200

    Bug fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const GithubApi = require('./GithubApi');
 
-const codiceFiscaleRegexp = /^(?:[B-DF-HJ-NP-TV-Z](?:[AEIOU]{2}|[AEIOU]X)|[AEIOU]{2}X|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[1256LMRS][\dLMNP-V])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[\dLMNP-V][1-9MNP-V]|[1-9MNP-V][0L]))[A-Z]$/g;
-const numeroDiTelefonoRegexp = /^(\((00|\+)39\)|(00|\+)39)?(38[890]|34[7-90]|36[680]|33[3-90]|32[89])\d{7}$/g;
+const codiceFiscaleRegexp = /(?:[B-DF-HJ-NP-TV-Z](?:[AEIOU]{2}|[AEIOU]X)|[AEIOU]{2}X|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[1256LMRS][\dLMNP-V])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[\dLMNP-V][1-9MNP-V]|[1-9MNP-V][0L]))[A-Z]/g;
+const numeroDiTelefonoRegexp = /(\((00|\+)39\)|(00|\+)39)?(38[890]|34[7-90]|36[680]|33[3-90]|32[89])\d{7}/g;
 
 const app = new express();
 
@@ -12,35 +12,37 @@ app.use(bodyParser.json());
 app.post('/github', (req, res) => {
   const { body } = req;
 
+  //Pubblicazione di un commento a una issue
   if(req.header('X-GitHub-Event') !== 'issue_comment') {
     if(body.action === 'deleted') {
       //Nothing to do if issue comment is deleted
-      return res.send();
+      return res.sendStatus(200);
     }
 
     return issueCommentPublishedOrUpdated(body)
-      .then(() => res.send())
+      .then(() => res.sendStatus(200))
       .catch((err) => {
         console.error('Message validations on issueCommentPublishedOrUpdated failed with exception: ', err);
         res.send();
       });
   }
 
+  //Pubblicazione issue
   if(req.header('X-GitHub-Event') !== 'issues') {
     if(body.action !== 'opened') {
       //Nothing to for other events
-      return res.send();
+      return res.sendStatus(200);
     }
 
     return issueCreated(body)
-      .then(() => res.send())
+      .then(() => res.sendStatus(200))
       .catch((err) => {
         console.error('Message validations on issueCommentPublishedOrUpdated failed with exception: ', err);
-        res.send();
+        res.sendStatus(200);
       });
   }
 
-  console.log('anpr-github-privacy-check has been subscribed to a wrong event. Please only select issue_comment');
+  console.log('anpr-github-privacy-check has been subscribed to a wrong event. Please only select issue_comment and issues');
   return res.send();
 });
 
@@ -50,7 +52,7 @@ function issueCreated(body) {
   return processIssueText(body.issue.body, body.issue, body.issue.user)
     .then(newMessage => {
       if(newMessage) {
-        GithubApi.updateIssueBody(body.issue, newMessage);
+        return GithubApi.updateIssueBody(body.issue, newMessage);
       }
     });
 }
@@ -59,7 +61,7 @@ function issueCommentPublishedOrUpdated(body) {
   return processIssueText(body.comment.body, body.issue, body.sender)
     .then(newMessage => {
       if(newMessage) {
-        GithubApi.updateIssueCommentBody(body.issue, newMessage);
+        return GithubApi.updateIssueCommentBody(body.issue, newMessage);
       }
     });
 }
